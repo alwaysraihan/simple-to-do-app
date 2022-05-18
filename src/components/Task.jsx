2835d62b256b23bb2c5dbf4c9b058de0d06257b5
Raihan Sarkar <bdyoungdeveloper@gmail.com>
2022-05-19 03:41:34 +0600

    readme file update

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,16 +1,74 @@
-import React from "react";
+import { async } from "@firebase/util";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { toast } from "react-toastify";
+import auth from "../Firebase/firebase.init";
 
 const Task = () => {
+    const [user] = useAuthState(auth);
+    const [myTasks, setMyTasks] = useState([]);
+    let todo = React.createRef();
+    const [load, setload] = useState(false);
     const addItem = (e) => {
+        const email = user.email;
         e.preventDefault();
+        const todoTask = todo.current.value;
+
+        const todoItem = { email, todoTask };
+
+        // post data to server
+
+        fetch("https://my-ossam-to-do-server.herokuapp.com/tasks", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify(todoItem),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data) {
+                    setload(!load);
+                }
+            });
+    };
+    useEffect(() => {
+        const getData = async () => {
+            const response = await fetch(
+                `https://my-ossam-to-do-server.herokuapp.com/tasks?email=${user.email}`
+            );
+            const data = await response.json();
+
+            setMyTasks(data);
+        };
+        getData();
+    }, [user, load, todo]);
+    const handleDelete = (id) => {
+        const proceed = window.confirm("Are you sure, you want to delete?");
+        if (proceed) {
+            const url = `https://my-ossam-to-do-server.herokuapp.com/tasks/${id}`;
+            fetch(url, {
+                method: "DELETE",
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    if (data.deletedCount > 0) {
+                        toast.success("deleted successfully");
+                        const remainingUsers = myTasks.filter(
+                            (task) => task._id !== id
+                        );
+                        setMyTasks(remainingUsers);
+                    }
+                });
+        }
     };
     return (
         <>
             <div className="px-5 md:px-10 lg:px-[10%] my-10 xl:px-[15%] ">
                 <div className="bg-white shadow-md rounded overflow-hidden">
                     <div>
-                        <div class="overflow-x-auto">
-                            <table class="table w-full">
+                        <div className="overflow-x-auto">
+                            <table className="table w-full">
                                 <thead className="px-5">
                                     <tr>
                                         <th></th>
@@ -19,15 +77,27 @@ const Task = () => {
                                     </tr>
                                 </thead>
                                 <tbody className="px-5 ">
-                                    <tr className="justify-center items-center">
-                                        <th>1</th>
-                                        <td className="w-full">Cy Ganderton</td>
-                                        <td className="flex-shrink-0 center">
-                                            <button className="flex-shrink-0 bg-red-500 hover:bg-teal-700 border-red-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 ml-2 rounded">
-                                                Delete
-                                            </button>
-                                        </td>
-                                    </tr>
+                                    {myTasks?.map((task) => (
+                                        <tr
+                                            className="justify-center items-center"
+                                            key={task._id}
+                                        >
+                                            <th>1</th>
+                                            <td className="w-full">
+                                                {task.todoTask}
+                                            </td>
+                                            <td className="flex-shrink-0 center">
+                                                <button
+                                                    onClick={() =>
+                                                        handleDelete(task._id)
+                                                    }
+                                                    className="flex-shrink-0 bg-red-500 hover:bg-teal-700 border-red-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 ml-2 rounded"
+                                                >
+                                                    Delete
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    ))}
                                 </tbody>
                             </table>
                         </div>
@@ -47,14 +117,15 @@ const Task = () => {
                             <input
                                 type="text"
                                 className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                id="item"
-                                name="item"
+                                id="todo"
+                                name="todo"
+                                ref={todo}
                                 placeholder="Pick up groceries"
                             />
 
                             <input
                                 type="submit"
-                                value="Add Item"
+                                value="Add To Do"
                                 className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 ml-2 rounded"
                             />
                         </div>
--- src/components/shared/Loading.jsx
+++ src/components/shared/Loading.jsx
@@ -4,7 +4,7 @@ const Loading = () => {
     return (
         <>
             <div className="flex justify-center items-center">
-                <button type="button" class=" mt-5  ..." disabled>
+                <button type="button" className=" mt-5  ..." disabled>
                     <img
                         src="https://i.pinimg.com/originals/f9/41/ae/f941ae9d16fd7d2957eea6e5b1100d1e.gif"
                         alt="loading spinner"
